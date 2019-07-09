dd1686e51f1548ba5dff94a4fdd2fa993ab96f5c
David <contact.davidrobert@gmail.com>
2019-07-09 14:17:13 +0200

    exo list article avec chemin + wildcard

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -155,13 +155,99 @@ class HomeController extends AbstractController
 
 
 	/**
-	 * @Route("/linktwig", name="link_twig")
+	 * @Route("/list", name="list")
 	 *
 	 * créé une page pour utiliser la fonction path et la fonction link
 	 */
-	public function linkTwig()
+	public function list()
 	{
-		return $this->render("linkTwig.html.twig");
+
+		$articles = [
+			1 => [
+				'title' => 'Article 1',
+				'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum explicabo fuga ipsa nostrum officiis placeat quas quisquam ullam unde vitae. Blanditiis dolore enim perspiciatis quos rerum similique sint ut? Laboriosam.',
+				'image' => 'https://gal.img.pmdstatic.net/fit/http.3A.2F.2Fprd2-bone-image.2Es3-website-eu-west-1.2Eamazonaws.2Ecom.2Fprismamedia_people.2F2017.2F07.2F03.2Fcd76a89a-9f7f-44fc-a066-06bbb34281af.2Ejpeg/2216x1536/quality/80/charles-pasqua.jpeg'
+			],
+			2 => [
+				'title' => 'Article 2',
+				'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum explicabo fuga ipsa nostrum officiis placeat quas quisquam ullam unde vitae. Blanditiis dolore enim perspiciatis quos rerum similique sint ut? Laboriosam.',
+				'image' => 'http://s1.lprs1.fr/images/2018/05/15/7716630_a7834498-5761-11e8-aba9-269965d92401-1_940x500.jpg'
+			],
+			3 => [
+				'title' => 'Article 3',
+				'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum explicabo fuga ipsa nostrum officiis placeat quas quisquam ullam unde vitae. Blanditiis dolore enim perspiciatis quos rerum similique sint ut? Laboriosam.',
+				'image' => 'https://tel.img.pmdstatic.net/fit/http.3A.2F.2Fprd2-bone-image.2Es3-website-eu-west-1.2Eamazonaws.2Ecom.2FTEL.2Enews.2F2018.2F01.2F12.2F28453de3-9265-4ba8-9b3d-1dae3e693d03.2Ejpeg/1150x495/crop-from/top/une-nouvelle-emission-pour-jamy-gourmaud.jpg'
+			],
+			4 => [
+				'title' => 'Article 4',
+				'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum explicabo fuga ipsa nostrum officiis placeat quas quisquam ullam unde vitae. Blanditiis dolore enim perspiciatis quos rerum similique sint ut? Laboriosam.',
+				'image' => 'https://static.cnews.fr/sites/default/files/styles/image_640_360/public/jean-lassalle_0.jpg?itok=kzxd2dc3'
+			],
+			5 => [
+				'title' => 'Article 5',
+				'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum explicabo fuga ipsa nostrum officiis placeat quas quisquam ullam unde vitae. Blanditiis dolore enim perspiciatis quos rerum similique sint ut? Laboriosam.',
+				'image' => 'https://www.lamanchelibre.fr/photos/maxi/649833.jpg'
+			],
+			6 => [
+				'title' => 'Article 6',
+				'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum explicabo fuga ipsa nostrum officiis placeat quas quisquam ullam unde vitae. Blanditiis dolore enim perspiciatis quos rerum similique sint ut? Laboriosam.',
+				'image' => 'https://img.cdandlp.com/2016/10/imgL/118348804.jpg'
+			],
+
+		];
+
+
+		return $this->render("articleList.html.twig" , [
+			'articles' => $articles
+		]);
+
+
+	}
+
+	/**
+	 * @Route("/single/{id}", name="single_article")
+	 */
+	public function single($id)
+	{
+
+		$articles = [
+			1 => [
+				'title' => 'Article 1',
+				'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum explicabo fuga ipsa nostrum officiis placeat quas quisquam ullam unde vitae. Blanditiis dolore enim perspiciatis quos rerum similique sint ut? Laboriosam.',
+				'image' => 'https://gal.img.pmdstatic.net/fit/http.3A.2F.2Fprd2-bone-image.2Es3-website-eu-west-1.2Eamazonaws.2Ecom.2Fprismamedia_people.2F2017.2F07.2F03.2Fcd76a89a-9f7f-44fc-a066-06bbb34281af.2Ejpeg/2216x1536/quality/80/charles-pasqua.jpeg'
+			],
+			2 => [
+				'title' => 'Article 2',
+				'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum explicabo fuga ipsa nostrum officiis placeat quas quisquam ullam unde vitae. Blanditiis dolore enim perspiciatis quos rerum similique sint ut? Laboriosam.',
+				'image' => 'http://s1.lprs1.fr/images/2018/05/15/7716630_a7834498-5761-11e8-aba9-269965d92401-1_940x500.jpg'
+			],
+			3 => [
+				'title' => 'Article 3',
+				'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum explicabo fuga ipsa nostrum officiis placeat quas quisquam ullam unde vitae. Blanditiis dolore enim perspiciatis quos rerum similique sint ut? Laboriosam.',
+				'image' => 'https://tel.img.pmdstatic.net/fit/http.3A.2F.2Fprd2-bone-image.2Es3-website-eu-west-1.2Eamazonaws.2Ecom.2FTEL.2Enews.2F2018.2F01.2F12.2F28453de3-9265-4ba8-9b3d-1dae3e693d03.2Ejpeg/1150x495/crop-from/top/une-nouvelle-emission-pour-jamy-gourmaud.jpg'
+			],
+			4 => [
+				'title' => 'Article 4',
+				'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum explicabo fuga ipsa nostrum officiis placeat quas quisquam ullam unde vitae. Blanditiis dolore enim perspiciatis quos rerum similique sint ut? Laboriosam.',
+				'image' => 'https://static.cnews.fr/sites/default/files/styles/image_640_360/public/jean-lassalle_0.jpg?itok=kzxd2dc3'
+			],
+			5 => [
+				'title' => 'Article 5',
+				'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum explicabo fuga ipsa nostrum officiis placeat quas quisquam ullam unde vitae. Blanditiis dolore enim perspiciatis quos rerum similique sint ut? Laboriosam.',
+				'image' => 'https://www.lamanchelibre.fr/photos/maxi/649833.jpg'
+			],
+			6 => [
+				'title' => 'Article 6',
+				'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum explicabo fuga ipsa nostrum officiis placeat quas quisquam ullam unde vitae. Blanditiis dolore enim perspiciatis quos rerum similique sint ut? Laboriosam.',
+				'image' => 'https://img.cdandlp.com/2016/10/imgL/118348804.jpg'
+			],
+
+		];
+
+		return $this->render('article_show.html.twig', [
+			'article' => $articles[$id]
+		]);
+
 	}
 
 
