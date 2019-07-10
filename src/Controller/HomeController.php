cb4f778527d4ef1415a904f420cc287c33f71f3d
David <contact.davidrobert@gmail.com>
2019-07-10 14:51:02 +0200

    exo extends + include

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -244,6 +244,59 @@ class HomeController extends AbstractController
 
 		];
 
+
+		$products = [
+			[
+				'title' => 'Trotinette 1',
+				'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum explicabo fuga ipsa nostrum officiis placeat quas quisquam ullam unde vitae. Blanditiis dolore enim perspiciatis quos rerum similique sint ut? Laboriosam.',
+				'image' => 'https://images.oxybul.com/Photo/IMG_FICHE_PRODUIT/Image/500x500/3/324168.jpg',
+				'id' => 1
+			],
+			[
+				'title' => 'Trotinette 2',
+				'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum explicabo fuga ipsa nostrum officiis placeat quas quisquam ullam unde vitae. Blanditiis dolore enim perspiciatis quos rerum similique sint ut? Laboriosam.',
+				'image' => 'https://images.oxybul.com/Photo/IMG_FICHE_PRODUIT/Image/500x500/3/324168.jpg',
+				'id' => 2
+			],
+			[
+				'title' => 'Trotinette 3',
+				'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum explicabo fuga ipsa nostrum officiis placeat quas quisquam ullam unde vitae. Blanditiis dolore enim perspiciatis quos rerum similique sint ut? Laboriosam.',
+				'image' => 'https://images.oxybul.com/Photo/IMG_FICHE_PRODUIT/Image/500x500/3/324168.jpg',
+				'id' => 3
+			],
+			[
+				'title' => 'Trotinette 4',
+				'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum explicabo fuga ipsa nostrum officiis placeat quas quisquam ullam unde vitae. Blanditiis dolore enim perspiciatis quos rerum similique sint ut? Laboriosam.',
+				'image' => 'https://images.oxybul.com/Photo/IMG_FICHE_PRODUIT/Image/500x500/3/324168.jpg',
+				'id' => 4
+			],
+			[
+				'title' => 'Trotinette 5',
+				'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum explicabo fuga ipsa nostrum officiis placeat quas quisquam ullam unde vitae. Blanditiis dolore enim perspiciatis quos rerum similique sint ut? Laboriosam.',
+				'image' => 'https://images.oxybul.com/Photo/IMG_FICHE_PRODUIT/Image/500x500/3/324168.jpg',
+				'id' => 5
+			],
+			[
+				'title' => 'Trotinette 6',
+				'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum explicabo fuga ipsa nostrum officiis placeat quas quisquam ullam unde vitae. Blanditiis dolore enim perspiciatis quos rerum similique sint ut? Laboriosam.',
+				'image' => 'https://images.oxybul.com/Photo/IMG_FICHE_PRODUIT/Image/500x500/3/324168.jpg',
+				'id' => 6
+			],
+			[
+				'title' => 'Trotinette 7',
+				'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum explicabo fuga ipsa nostrum officiis placeat quas quisquam ullam unde vitae. Blanditiis dolore enim perspiciatis quos rerum similique sint ut? Laboriosam.',
+				'image' => 'https://images.oxybul.com/Photo/IMG_FICHE_PRODUIT/Image/500x500/3/324168.jpg',
+				'id' => 7
+			],
+			[
+				'title' => 'Trotinette 8',
+				'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum explicabo fuga ipsa nostrum officiis placeat quas quisquam ullam unde vitae. Blanditiis dolore enim perspiciatis quos rerum similique sint ut? Laboriosam.',
+				'image' => 'https://images.oxybul.com/Photo/IMG_FICHE_PRODUIT/Image/500x500/3/324168.jpg',
+				'id' => 8
+			],
+
+		];
+
 		return $this->render('article_show.html.twig', [
 			'article' => $articles[$id]
 		]);
